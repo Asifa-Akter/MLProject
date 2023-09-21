07d3026e7747dcd1b242c5946794c7a3b16996bf
Asifa Akter <asifa.akter@studenti.unipd.it>
2023-09-21 16:59:38 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str) -> List[str]:
+    
+    '''
+     this function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+       requirements = file_obj.readlines()
+       [req.replace('\n', ' ') for req in requirements]
+       
+       if HYPEN_E_DOT in requirements:
+           requirements.remove(HYPEN_E_DOT)
+    return requirements
+    
+
+setup(
+    name = 'mlProject',
+    version = '0.1',
+    packages = find_packages(),
+    url = 'https://github.com/mlProject/mlProject',
+    license = 'MIT',
+    author = 'ASIFA AKTER',
+    author_email = 'asifa.akter@studenti.unipd.it',
+    description = 'Python Machine Learning Project',
+    install_requires = get_requirements('requirements.txt'),
+)
+    
